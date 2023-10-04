6a9bf17167de33da3b484250e5cff5143355d5f1
Lenovo-User <Lenovo-User@DESKTOP-RNPFAS5>
2023-10-04 15:13:09 +0200

    Add project files.

---
+++ 10. BMI calculator/Form1.vb
@@ -0,0 +1,31 @@
+﻿Imports System.Diagnostics.Eventing.Reader
+
+Public Class Form1
+	Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+		'Declaration of variables
+		Dim height, weight, bmi As Double
+
+		weight = Val(TextBox1.Text)
+		height = Val(TextBox2.Text)
+
+		bmi = (weight / (height) ^ 2).ToString("F2") ' BMI formula rounder to 2 decimal places
+
+		TextBox3.Text = bmi
+
+		If bmi < 18 Then
+			TextBox4.Text = "You are underweight"
+
+		ElseIf 18 <= bmi And bmi < 26 Then
+			TextBox4.Text = "You are normal"
+
+		Else
+			TextBox4.Text = "you are overweight"
+
+
+		End If
+
+
+
+	End Sub
+End Class
